1f345da9622fe0131622d5d3f95f705dc4fa7289
Paul Dreik <github@pauldreik.se>
2024-08-04 16:46:27 +0200

    DONT MERGE THIS nice changes and additions for developing the fuzzers

---
+++ fuzz/README.md
@@ -0,0 +1,126 @@
+# Fuzzing
+
+Fuzzing is easiest to run on Debian or Ubuntu.
+Make sure you have clang installed.
+
+  * clang
+  * xxd
+  * ninja
+  * cmake
+
+
+## Running 
+
+
+    cd fuzz
+    ./build.sh
+    mkdir -p corpus/conversion
+    out/conversion corpus/conversion
+
+
+## Minimization of a crash
+In case a crash is found, do
+
+    ./minimize_and_cleanse.sh out/conversion crash-*
+
+You can turn it into a test case with
+
+    PRINT_FUZZ_CASE= out/conversion cleaned_crash.conversion >blah 2>&1
+
+## Finding functions to fuzz
+
+    // this list was obtained by manually copying the interesting part of implementation.cpp ("class implementation") into impl.cpp and running
+    // clang-17 -Xclang -ast-dump  impl.cpp  2>/dev/null|sed -e 's/\x1b\[[0-9;]*m//g' | grep -- CXXMethodDecl >tjoff
+    // cat tjoff |grep --only-matching convert.* |cut -f1 -d' ' |sort  |xargs -I§ echo "ADD( " § "),"
+
+
+        // these require valid input so not suitable to fuzz unless validating input before passing it
+        // ADD(convert_valid_utf16be_to_latin1),
+        // ADD(convert_valid_utf16be_to_utf32),
+        // ADD(convert_valid_utf16be_to_utf8),
+        // ADD(convert_valid_utf16le_to_latin1),
+        // ADD(convert_valid_utf16le_to_utf32),
+        // ADD(convert_valid_utf16le_to_utf8),
+        // ADD(convert_valid_utf32_to_latin1),
+        // ADD(convert_valid_utf32_to_utf16be),
+        // ADD(convert_valid_utf32_to_utf16le),
+        // ADD(convert_valid_utf32_to_utf8),
+        // ADD(convert_valid_utf8_to_latin1),
+        // ADD(convert_valid_utf8_to_utf16be),
+        // ADD(convert_valid_utf8_to_utf16le),
+        // ADD(convert_valid_utf8_to_utf32),
+
+        // these have other function signatures
+        // ADD(base64_length_from_binary),
+        // ADD(base64_to_binary),
+        // ADD(base64_to_binary),
+        // ADD(binary_to_base64),
+        // ADD(change_endianness_utf16),
+        // ADD(count_utf16be),
+        // ADD(count_utf16le),
+        // ADD(count_utf8),
+        // ADD(latin1_length_from_utf16),
+        // ADD(latin1_length_from_utf32),
+        // ADD(latin1_length_from_utf8),
+        // ADD(maximal_binary_length_from_base64),
+        // ADD(maximal_binary_length_from_base64),
+        // ADD(utf16_length_from_latin1),
+        // ADD(utf16_length_from_utf32),
+        // ADD(utf16_length_from_utf8),
+        // ADD(utf32_length_from_latin1),
+        // ADD(utf32_length_from_utf16be),
+        // ADD(utf32_length_from_utf16le),
+        // ADD(utf32_length_from_utf8),
+        // ADD(utf8_length_from_latin1),
+        // ADD(utf8_length_from_utf16be),
+        // ADD(utf8_length_from_utf16le),
+        // ADD(utf8_length_from_utf32),
+
+        // already fuzzed
+        // ADD(validate_utf16be),
+        // ADD(validate_utf16be_with_errors),
+        // ADD(validate_utf16le),
+        // ADD(validate_utf16le_with_errors),
+        // ADD(validate_utf32),
+        // ADD(validate_utf32_with_errors),
+        // ADD(validate_utf8),
+        // ADD(validate_utf8_with_errors)
+
+## test case generation
+
+make the reproduce build:
+export CXX=/usr/lib/ccache/clang++-18
+cmake -B /tmp/reproduce -S .. -DSIMDUTF_SANITIZE=On -DSIMDUTF_SANITIZE_UNDEFINED=On -DCMAKE_BUILD_TYPE=Debug -DSIMDUTF_ALWAYS_INCLUDE_FALLBACK=On -GNinja
+
+### conversion
+rm -f crash-*
+./build.sh && out/conversion ../corpus/conversion/
+./build.sh && out/conversion -jobs=15 -workers=15 ../corpus/conversion/
+./build.sh && ./minimize_and_cleanse.sh out/conversion crash-*
+./build.sh && PRINT_FUZZ_CASE= out/conversion cleaned_crash.conversion >blah 2>&1 
+sed  -n '/begin testcase/,/end testcase/p' blah >> ../tests/fuzzercrashes.cpp
+(echo '/*';sed  -n '/begin errormessage/,/end errormessage/p' blah ; echo '*/') >> ../tests/fuzzercrashes.cpp
+mv crash-* ../corpus/conversion
+cp cleaned_crash.conversion ../corpus/conversion/$(echo cleaned_crash.conversion$RANDOM)
+ninja -C /tmp/reproduce && /tmp/reproduce/tests/fuzzercrashes
+rm -f crash-*
+
+### conversion_valid
+rm -f crash-*
+./build.sh && out/conversion_valid ../corpus/conversion_valid/
+./build.sh && ./minimize_and_cleanse.sh out/conversion_valid crash-*
+./build.sh && out/conversion_valid cleaned_crash.conversion_valid >blah 2>&1 
+sed  -n '/begin testcase/,/end testcase/p' blah >> ../tests/fuzzercrashes.cpp
+(echo '/*';sed  -n '/begin errormessage/,/end errormessage/p' blah ; echo '*/') >> ../tests/fuzzercrashes.cpp
+mv crash-* ../corpus/conversion_valid
+cp cleaned_crash.conversion_valid ../corpus/conversion_valid/$(echo cleaned_crash.conversion_valid$RANDOM)
+ninja -C /tmp/reproduce && /tmp/reproduce/tests/fuzzercrashes
+
+
+## More stuff to try out
+
+Test alignment, slide the fuzzer data and make sure it still works.
+
+test on arm.
+
+test the output from conversion to make sure it is the same between implementations
---
+++ tests/fuzzercrashes.cpp
@@ -0,0 +1,349 @@
+#include "simdutf.h"
+
+#include <vector>
+
+#include <tests/helpers/random_int.h>
+#include <tests/helpers/test.h>
+#include <tests/helpers/transcode_test_base.h>
+
+TEST_MAIN
+
+#define IGNORE return
+
+// begin testcase
+TEST(issue_convert_utf16le_to_latin1_with_errors_cbf29ce48422238a)
+{
+    IGNORE;
+    const unsigned char data[] = {0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char16_t);
+    std::vector<char> output(4 * data_len);
+    const auto r = implementation.convert_utf16le_to_latin1_with_errors((const char16_t *) data,
+                                                                        data_len,
+                                                                        output.data());
+    /*
+    got return [count=0, error=TOO_LARGE] from implementation icelake
+    got return [count=0, error=TOO_LARGE] from implementation haswell
+    got return [count=8, error=SUCCESS] from implementation westmere
+    got return [count=0, error=TOO_LARGE] from implementation fallback
+    */
+    ASSERT_EQUAL(r.count, 0);
+    ASSERT_EQUAL(r.error, simdutf::error_code::TOO_LARGE);
+}
+
+TEST(issue_convert_utf16be_to_latin1_with_errors_cbf29ce484222384)
+{
+    IGNORE;
+    const unsigned char data[] = {0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00,
+                                  0x20,
+                                  0x00};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char16_t);
+    std::vector<char> output(4 * data_len);
+    const auto r = implementation.convert_utf16be_to_latin1_with_errors((const char16_t *) data,
+                                                                        data_len,
+                                                                        output.data());
+    /*
+    got return [count=0, error=TOO_LARGE] from implementation icelake
+    got return [count=0, error=TOO_LARGE] from implementation haswell
+    got return [count=8, error=SUCCESS] from implementation westmere
+    got return [count=0, error=TOO_LARGE] from implementation fallback
+    */
+
+    ASSERT_EQUAL(r.count, 0);
+    ASSERT_EQUAL(r.error, simdutf::error_code::TOO_LARGE);
+}
+
+TEST(issue_convert_utf32_to_utf8_with_errors_cbf29ce484222315)
+{
+    IGNORE;
+    const unsigned char data[] = {0x20, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00,
+                                  0x00, 0x20, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x20, 0x00,
+                                  0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x20,
+                                  0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x80,
+                                  0x20, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00,
+                                  0x00, 0x20, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char32_t);
+    std::vector<char> output(4 * data_len);
+    const auto r = implementation.convert_utf32_to_utf8_with_errors((const char32_t *) data,
+                                                                    data_len,
+                                                                    output.data());
+    /*
+    got return [count=10, error=TOO_LARGE] from implementation icelake
+    got return [count=10, error=TOO_LARGE] from implementation haswell
+    got return [count=16, error=TOO_LARGE] from implementation westmere
+    got return [count=10, error=TOO_LARGE] from implementation fallbackend errormessage
+    */
+    ASSERT_EQUAL(r.count, 10);
+    ASSERT_EQUAL(r.error, simdutf::error_code::TOO_LARGE);
+}
+
+TEST(issue_convert_utf8_to_latin1_with_errors_cbf29ce4842223ed)
+{
+    IGNORE;
+    const unsigned char data[] = {0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0xc2};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char);
+    std::vector<char> output(4 * data_len);
+    const auto r = implementation.convert_utf8_to_latin1_with_errors((const char *) data,
+                                                                     data_len,
+                                                                     output.data());
+    /*
+    got return [count=63, error=SUCCESS] from implementation icelake
+    got return [count=63, error=TOO_SHORT] from implementation haswell
+    got return [count=63, error=TOO_SHORT] from implementation westmere
+    got return [count=63, error=TOO_SHORT] from implementation fallback
+    */
+    ASSERT_EQUAL(r.count, 63);
+    ASSERT_EQUAL(r.error, simdutf::error_code::TOO_SHORT);
+}
+
+TEST(issue_convert_utf8_to_latin1_cbf29ce4842223c9)
+{
+    IGNORE;
+    const unsigned char data[] = {0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0xc2, 0xbd, 0xc2, 0x90, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0xc2};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char);
+    std::vector<char> output(4 * data_len);
+    const auto r = implementation.convert_utf8_to_latin1((const char *) data,
+                                                         data_len,
+                                                         output.data());
+    /*
+    got return 61 from implementation icelake
+    got return 0 from implementation haswell
+    got return 0 from implementation westmere
+    got return 0 from implementation fallback
+    */
+    ASSERT_EQUAL(r, 0);
+}
+
+TEST(issue_convert_valid_utf8_to_latin1_cbf29ce4842223f0)
+{
+    IGNORE;
+    const unsigned char data[] = {0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0xff};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char);
+    const auto validation1 = implementation.validate_utf8_with_errors((const char *) data, data_len);
+    /*
+    got return [count=64, error=SUCCESS] from implementation icelake
+    got return [count=63, error=HEADER_BITS] from implementation haswell
+    got return [count=63, error=HEADER_BITS] from implementation westmere
+    got return [count=63, error=HEADER_BITS] from implementation fallback
+    */
+    ASSERT_EQUAL(validation1.count, 63);
+    ASSERT_EQUAL(validation1.error, simdutf::error_code::HEADER_BITS);
+
+    if (validation1.error != simdutf::error_code::SUCCESS) {
+        return;
+    }
+
+    std::vector<char> output(4 * data_len);
+    const auto r = implementation.convert_valid_utf8_to_latin1((const char *) data,
+                                                               data_len,
+                                                               output.data());
+    ASSERT_EQUAL(r, 1234);
+}
+
+TEST(issue_convert_utf16be_to_latin1_with_errors_xyz)
+{
+    IGNORE;
+    const unsigned char data[] = {0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00,
+                                  0x20, 0x00, 0x20, 0x00, 0x20, 0x00, 0x20, 0x00, 0x20, 0x00, 0x20,
+                                  0x00, 0x20, 0x00, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char16_t);
+    std::vector<char> output(4 * data_len);
+    const auto r = implementation.convert_utf16be_to_latin1_with_errors((const char16_t *) data,
+                                                                        data_len,
+                                                                        output.data());
+    /*
+    got return [count=13, error=TOO_LARGE] from implementation icelake
+    got return [count=13, error=TOO_LARGE] from implementation haswell
+    got return [count=13, error=TOO_LARGE] from implementation westmere
+    got return [count=16, error=SUCCESS] from implementation fallback
+    */
+    ASSERT_EQUAL(r.count, 13);
+    ASSERT_EQUAL(r.error, simdutf::error_code::TOO_LARGE);
+}
+
+TEST(issue_convert_utf8_to_utf32_with_errors_a8ec246845d4878e)
+{
+    IGNORE;
+    const unsigned char data[] = {0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0xf2, 0xa8, 0xa4, 0x8b, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0xf2, 0xa8,
+                                  0xa4, 0x8b, 0x20, 0x20, 0x20, 0x20, 0xf2, 0xa8, 0xa4, 0x8b, 0x20,
+                                  0x20, 0xf2, 0xa8, 0xa4, 0x8b, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0xf2, 0xa8, 0xa4, 0xa8, 0xa4, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+                                  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char);
+    std::vector<char32_t> output(4 * data_len);
+    const auto r = implementation.convert_utf8_to_utf32_with_errors((const char *) data,
+                                                                    data_len,
+                                                                    output.data());
+    /*
+    got return [count=61, error=TOO_LONG] from implementation icelake
+    got return [count=64, error=TOO_LONG] from implementation haswell
+    got return [count=64, error=TOO_LONG] from implementation westmere
+    got return [count=64, error=TOO_LONG] from implementation fallback
+    */
+    ASSERT_EQUAL(r.count, 64);
+    ASSERT_EQUAL(r.error, simdutf::error_code::TOO_LONG);
+}
+
+TEST(issue_convert_valid_utf16be_to_latin1_0dce64acfa99c657)
+{
+    IGNORE;
+    const unsigned char data[] = {0x20, 0x20};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char16_t);
+    const auto validation1 = implementation.validate_utf16be_with_errors((const char16_t *) data,
+                                                                         data_len);
+    ASSERT_EQUAL(validation1.count, 1);
+    ASSERT_EQUAL(validation1.error, simdutf::error_code::SUCCESS);
+
+    if (validation1.error != simdutf::error_code::SUCCESS) {
+        return;
+    }
+    const auto outlen = implementation.latin1_length_from_utf16(data_len);
+    ASSERT_EQUAL(outlen, 1);
+    std::vector<char> output(outlen);
+    const auto r = implementation.convert_valid_utf16be_to_latin1((const char16_t *) data,
+                                                                  data_len,
+                                                                  output.data());
+    /*
+    got return [retval=0, output hash=c27133214e39168d] from implementation icelake
+    got return [retval=0, output hash=c27133214e39168d] from implementation haswell
+    got return [retval=0, output hash=c27133214e39168d] from implementation westmere
+    got return [retval=1, output hash=e429605e1c6ed6a3] from implementation fallback
+    */
+    ASSERT_EQUAL(r, 0);
+}
+
+TEST(issue_convert_valid_utf16le_to_latin1_903167bc5e26f433)
+{
+    IGNORE;
+    const unsigned char data[] = {0x20, 0x20};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char16_t);
+    const auto validation1 = implementation.validate_utf16le_with_errors((const char16_t *) data,
+                                                                         data_len);
+    ASSERT_EQUAL(validation1.count, 1);
+    ASSERT_EQUAL(validation1.error, simdutf::error_code::SUCCESS);
+
+    if (validation1.error != simdutf::error_code::SUCCESS) {
+        return;
+    }
+    const auto outlen = implementation.latin1_length_from_utf16(data_len);
+    ASSERT_EQUAL(outlen, 1);
+    std::vector<char> output(outlen);
+    const auto r = implementation.convert_valid_utf16le_to_latin1((const char16_t *) data,
+                                                                  data_len,
+                                                                  output.data());
+    /*
+    got return [retval=0, output hash=c27133214e39168d] from implementation icelake
+    got return [retval=0, output hash=c27133214e39168d] from implementation haswell
+    got return [retval=0, output hash=c27133214e39168d] from implementation westmere
+    got return [retval=1, output hash=e429605e1c6ed6a3] from implementation fallback
+    */
+    ASSERT_EQUAL(r, 0);
+}
+
+TEST(allow_empty_input)
+{
+    std::vector<char16_t> emptydata;
+    std::vector<char32_t> output(10);
+
+    auto ret = implementation.convert_utf16le_to_utf32_with_errors(emptydata.data(),
+                                                                   emptydata.size(),
+                                                                   output.data());
+    ASSERT_EQUAL(ret.error, simdutf::error_code::SUCCESS);
+}
+
+TEST(issue_convert_valid_utf8_to_latin1_d4b91ecf2c5f2158)
+{
+    IGNORE;
+    const unsigned char data[] = {0xdf, 0xaf};
+    constexpr std::size_t data_len_bytes = sizeof(data);
+    constexpr std::size_t data_len = data_len_bytes / sizeof(char);
+    const auto validation1 = implementation.validate_utf8_with_errors((const char *) data, data_len);
+    ASSERT_EQUAL(validation1.count, 2);
+    ASSERT_EQUAL(validation1.error, simdutf::error_code::SUCCESS);
+
+    if (validation1.error != simdutf::error_code::SUCCESS) {
+        return;
+    }
+    const auto outlen = implementation.latin1_length_from_utf8((const char *) data, data_len);
+    ASSERT_EQUAL(outlen, 1);
+    std::vector<char> output(outlen);
+    const auto r = implementation.convert_valid_utf8_to_latin1((const char *) data,
+                                                               data_len,
+                                                               output.data());
+    /*
+    got return [retval=1, output hash=fc4a1b7c7f7d51e5] from implementation icelake
+    got return [retval=1, output hash=55b34275898c5990] from implementation haswell
+    got return [retval=1, output hash=55b34275898c5990] from implementation westmere
+    got return [retval=1, output hash=55b34275898c5990] from implementation fallback
+    */
+    ASSERT_EQUAL(r, 1);
+    const std::vector<char> expected_out{-17};
+    ASSERT_TRUE(output.size() == expected_out.size());
+    for (std::size_t i = 0; i < output.size(); ++i) {
+        ASSERT_EQUAL(+output.at(i), +expected_out.at(i));
+    };
+}
